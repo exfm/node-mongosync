f03fb918f47bca81c97baba048422697e365b88f
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2012-10-25 16:10:11 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+"use strict";
+
+var leveldb = require('leveldb'),
+    express = require("express");
+
+var app = module.exports = express();
+
+// Configuration
+app.configure(function(){
+    app.use(app.router);
+});
+
+app.configure("development", function(){
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure("production", function(){
+    app.use(express.errorHandler());
+});
+
+var db;
+leveldb.open("mongosync.db", {create_if_missing: true}, function(err, _db){
+    db = _db;
+});
+
+app.get('/flush/:collection', function(req, res){
+    var items = [];
+    db.iterator({}, function(err, it){
+        it.forRange('song-', 'song-a', function(err, key, value){
+            items.push(JSON.parse(value));
+            db.del(key);
+        }, function(){
+            res.send(items);
+        });
+    });
+});
+
+app.all('/:collection/:id', function(req, res, next){
+    var key = [req.param('collection'), '-', req.param('id')].join(''),
+        val;
+
+    db.get(key, function(err, value){
+        if(value === null){
+            val = {
+                'first_seen': new Date().toString(),
+                'last_seen': new Date().toString(),
+                'hits': 1,
+                'id': Number(req.param('id'))
+            };
+        }
+        else{
+            val = JSON.parse(value);
+            val.last_seen = new Date().toString();
+            val.hits++;
+            val.id = Number(req.param('id'));
+        }
+        db.put(key, JSON.stringify(val), function(err) {
+            res.send(val);
+        });
+    });
+});
+
+app.listen(8080, function(err, item){
+    console.log('Syncer listening on 8080');
+});
